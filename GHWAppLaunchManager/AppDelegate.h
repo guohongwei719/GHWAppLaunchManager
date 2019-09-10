1dcd5b546fd54b3a63ba222b9540562049d7b8bd
guohongwei719 <guohongwei04441@hellobike.com>
2019-09-10 19:07:21 +0800

    fix

--- GHWAppLaunchManager/AppDelegate.h
+++ GHWAppLaunchManager/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  GHWAppLaunchManager
 //
-//  Created by 郭宏伟 on 2019/9/10.
-//  Copyright © 2019 Jingyao. All rights reserved.
+//  Created by 黑化肥发灰 on 2019/9/10.
+//  Copyright © 2019 黑化肥发灰. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
