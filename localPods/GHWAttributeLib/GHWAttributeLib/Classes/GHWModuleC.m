bbd0f02a365fe54c5edd9497f9683ab4ce4e2dcb
guohongwei719 <guohongwei04441@hellobike.com>
2019-09-17 15:02:02 +0800

    fix

--- localPods/GHWAttributeLib/GHWAttributeLib/Classes/GHWModuleC.m
+++ localPods/GHWAttributeLib/GHWAttributeLib/Classes/GHWModuleC.m
@@ -15,14 +15,7 @@ GHW_FUNCTION_EXPORT(pre_main)() {
     [[GHWModuleC sharedInstance] initMudule];
 }
 
-//GHW_FUNCTION_EXPORT(Stage_A)() {
-//    printf("ModuleC:Stage_A\n");
-//}
-
-static void _GHWStage_A(void); \
-__attribute__((used, section("__GHW,__Stage_A"))) \
-static const struct GHW_Function __FStage_A = (struct GHW_Function){(char *)(&("Stage_A")), (void *)(&_GHWStage_A)}; \
-static void _GHWStage_A () {
+GHW_FUNCTION_EXPORT(Stage_A)() {
     printf("ModuleC:Stage_A\n");
 }
 
